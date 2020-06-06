c619345c35aacecba625bc430021ad0f8ba147e1
Fabio Motta <fabiomotta0311@gmail.com>
2020-06-06 15:36:18 -0300

    some changes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import React from  'react';
+
+import './styles.css';
+
+import logo  from '../../assets/logo.svg';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta"/>
+                </header>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home;

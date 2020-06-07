d4986a40b3b7e88ff1e55e9a04e54ceff25023a8
Fabio Motta <fabiomotta0311@gmail.com>
2020-06-07 15:02:04 -0300

    a

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from  'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -11,7 +13,16 @@ const Home = () => {
                 <header>
                     <img src={logo} alt="Ecoleta"/>
                 </header>
-                
+                <main>
+                    <h1>Seu marktplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn/>
+                        </span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
+                </main>
             </div>
         </div>
     )
